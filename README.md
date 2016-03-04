39982b270127273eda2d9cd9ec5c935600121a7a
James Harding <jamesharding@memiah.co.uk>
2016-03-04 16:09:00 +0000

    Add instructions and suggestions to src files. Add README.md

---
+++ README.md
@@ -0,0 +1,55 @@
+# Micro-blog Technical test
+
+## About
+
+A part-implemented micro blogging app with API
+
+
+## Provides
+
+* Centos 7 Box
+* LEMP Stack
+  * Nginx
+  * PHP-FPM
+  * SQLite
+
+## Requires
+
+* Ansible 1.8.1+
+* Vagrant 1.8+
+* Internet access for additional software download and installs
+
+## Depends on
+
+List of project specific system requirements.
+* See requirements.yml
+
+## Setup
+
+### Install / Update Ansible-Galaxy Roles
+
+Run the following command to update your ansible galaxy roles (sudo if necessary):
+```
+ansible-galaxy install -r requirements.yml --ignore-errors
+```
+
+### Add following line to your host machine /etc/hosts:
+
+```
+192.168.100.120 micro-blog.dev
+```
+
+## Usage
+
+### Dev
+
+Start up the vagrant box:
+
+```
+vagrant up
+```
+
+### Nginx
+
+* visit 192.168.100.102 for Nginx test page
+* visit any of the URL's listed above in the provides section
--- src/index.php
+++ src/index.php
@@ -8,6 +8,7 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 
+// SQLite config
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver'   => 'pdo_sqlite',
@@ -15,12 +16,26 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     ),
 ));
 
+// Twig template engine config
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
 
-/* ------- micro-blog api --------- */
+/* ------- micro-blog api ---------
+
+All CRUD operations performed within our /api/ endpoints below
+
+TODO: Error checking - e.g. if try retrieve posts for a user_id that does
+      not exist, return an error message and an appropriate HTTP status code.
+
+      Implement /api/posts/new endpoint to add a new micro-blog post for a
+      given user.
+
+      Extra: Add new API endpoints for any extra features you can think of.
+
+      Extra: Improve on current API code where you see necessary
+*/
 
 $app->get('/api/posts', function() use($app) {
     $sql = "SELECT rowid, * FROM posts";
@@ -48,7 +63,16 @@ $app->post('/api/posts/new', function (Request $request) {
 });
 
 
-/* ------- micro-blog web app --------- */
+
+/* ------- micro-blog web app ---------
+
+All Endpoints for micro-blog web app below.
+
+TODO: Build front-end of web app in the / endpoint below - Add more
+      endpoints if you like.
+
+      See TODO in index.twig for more instructions / suggestions
+*/
 
 $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
